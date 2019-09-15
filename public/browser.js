ecf063afcc7aadd8270be8ef96a56f6c13a1a833
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-09-15 17:59:17 -0400

    initial commit

---
+++ public/browser.js
@@ -0,0 +1,22 @@
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('delete-me')){
+        if(confirm('Do you really want to delete it?')){
+            axios.post('/delete-item', {id: e.target.getAttribute('data-id')}).then(function(){
+                e.target.parentElement.parentElement.remove();
+            }).catch(function(){
+                console.log('Please try again later');
+            })
+        }
+    }
+
+   if(e.target.classList.contains('edit-me')){
+      let userInput = prompt('Enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML);
+      if(userInput){
+          axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(function(){
+                e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput;
+          }).catch(function(){
+              console.log("Please try again later");
+          });
+      }
+   }
+});
